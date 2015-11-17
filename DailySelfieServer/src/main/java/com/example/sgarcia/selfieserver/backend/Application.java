6ed363076c860ef04de03b32b588cc3c519471b2
Sergio Garcia <sgarcia@openmobileww.com>
2015-11-16 22:30:08 -0500

    GAE is able to deploy

---
+++ DailySelfieServer/src/main/java/com/example/sgarcia/selfieserver/backend/Application.java
@@ -0,0 +1,36 @@
+package com.example.sgarcia.selfieserver.backend;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by sgarcia on 11/1/2015.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class Application extends WebMvcConfigurerAdapter {
+//public class Application {
+    private static final String MAX_REQUEST_SIZE = "150MB";
+    /*@Bean
+    public MultipartConfigElement multipartConfigElement() {
+        // Setup the application container to be accept multipart requests
+        final MultipartConfigFactory factory = new MultipartConfigFactory();
+        // Place upper bounds on the size of the requests to ensure that
+        // clients don't abuse the web container by sending huge requests
+        factory.setMaxFileSize(MAX_REQUEST_SIZE);
+        factory.setMaxRequestSize(MAX_REQUEST_SIZE);
+
+        // Return the configuration to setup multipart in the container
+        return factory.createMultipartConfig();
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver=new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("utf-8");
+        return resolver;
+    }*/
+}
